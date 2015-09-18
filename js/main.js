3f4c07c9f2125aa515fd8e288c12c7b977e00056
Sergio Flores <sergioflores836@gmail.com>
2015-09-18 13:46:03 -0500

    got the geolocation working for html 5. figured out how to make the correct api call. broke location name

--- js/main.js
+++ js/main.js
@@ -1,11 +1,32 @@
 $(document).ready(function(){
 	$.ajax({
 		type: 'GET',
-		url: 'http://api.openweathermap.org/data/2.5/weather?q=DeKalb, IL',
+		url: "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + longi,
 		success: function(data){
 			console.log("success", data);
 			$('#temp').text(((data.main.temp) * (9/5) - 459.67) + ' F');
 			$('#city').text(data.name);
 		}
 	})
+	
+	var lat, longi = 0;
+	function getLocation() {
+	    if (navigator.geolocation) {
+	        navigator.geolocation.getCurrentPosition(showPosition);
+	    } 
+	    else { 
+	        $("#coordinates").text("Geolocation is not supported by this browser."); 
+	    }
+	}
+	
+	function showPosition(position) {
+	    $('#coordinates').html("Latitude: " + position.coords.latitude +
+	    "<br>Longitude: " + position.coords.longitude); 
+	    lat = position.coords.latitude;
+	    longi = position.coords.longitude;
+	    console.log(lat, longi);
+	}
+	getLocation();
 });
+
+
