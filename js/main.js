6691b9efa39e51aaf19e511dbe1b1441b01131d8
Sergio Flores <sergioflores836@gmail.com>
2015-09-18 14:20:34 -0500

    figured out how to form the correct api call with coordinates

--- js/main.js
+++ js/main.js
@@ -1,15 +1,6 @@
 $(document).ready(function(){
-	$.ajax({
-		type: 'GET',
-		url: "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + longi,
-		success: function(data){
-			console.log("success", data);
-			$('#temp').text(((data.main.temp) * (9/5) - 459.67) + ' F');
-			$('#city').text(data.name);
-		}
-	})
-	
-	var lat, longi = 0;
+	var lat = 0;
+	var longi = 0;
 	function getLocation() {
 	    if (navigator.geolocation) {
 	        navigator.geolocation.getCurrentPosition(showPosition);
@@ -22,11 +13,32 @@ $(document).ready(function(){
 	function showPosition(position) {
 	    $('#coordinates').html("Latitude: " + position.coords.latitude +
 	    "<br>Longitude: " + position.coords.longitude); 
+	    // lat = Math.round(position.coords.latitude);
+	    // longi = Math.round(position.coords.longitude);
 	    lat = position.coords.latitude;
 	    longi = position.coords.longitude;
-	    console.log(lat, longi);
+	    console.log("after setting coords" + lat, longi);
+	    //console.log("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + longi);
+	    newURL = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + longi;
+	    //console.log(newURL);
+
+	    $.ajax({
+		type: 'GET',
+		url: "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + longi,
+		success: function(data){
+			console.log("right after making api call " + lat, longi);
+			console.log("success", data);
+			$('#temp').text(((data.main.temp) * (9/5) - 459.67) + ' F');
+			$('#city').text(data.name);
+		}
+		})
 	}
-	getLocation();
+getLocation();
+console.log("right after making getLocation call " + lat, longi);	
+	
+	
+	
+	
 });
 
 
